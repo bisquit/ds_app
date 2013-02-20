c0cfce6f9ee973dacc2f9b20d8936c7a552de01f
Pluto <qu1dpr09u0@gmail.com>
2013-02-20 23:10:02 +0900

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+drum score creation service.
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DrumscoreApp::Application.config.secret_token = 'e6723d73532a8c90a8a5eeb24476a57da93ad69888f1f9ece985fe5445345d60331efb36c88016e4a4642b0cb280ff1cd060d0d3a49b6e24b0e2437b8e77136e'
