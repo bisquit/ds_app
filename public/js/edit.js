070653a4f405864546d13981a6b93dd916b74ca5
Pluto <qu1dpr09u0@gmail.com>
2013-02-23 02:42:57 +0900

    snare.mp3 -> .wav

--- public/js/edit.js
+++ public/js/edit.js
@@ -728,7 +728,7 @@ function prepareSound(){
         {src:"sounds/crash.wav", id:"crash"},
         {src:"sounds/hihat1.wav", id:"hihat"},
         {src:"sounds/tom.wav", id:"tom"},
-        {src:"sounds/snare1.mp3", id:"snare"},        
+        {src:"sounds/snare1.wav", id:"snare"},        
         {src:"sounds/floor.wav", id:"floor"},
         {src:"sounds/bass.wav", id:"bass"},
         /*{src:"sounds/ride1.wav", id:"ride"},*/
